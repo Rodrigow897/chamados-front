410c06d3d11f033d89af3bde342cf49a86dbdadb
Rodrigo <rwesley2000@gmail.com>
2025-08-20 14:19:52 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <div className="container">
+        <div id='titulo'>
+          <h1>Gestão de chamados</h1>
+        </div>
+
+        <section>
+         <p id='paragrafo'>Criar Novo Chamado</p> 
+         <div className="input-box">
+          <label id='title-input'>titulo</label>
+          <input type="text" placeholder='Informe o titulo' />
+         </div>
+
+         <div className="input-box">
+          <label>Profissional Responsavel</label>
+          <select id="select-input">
+            <option value="TI">TI</option>
+            <option value="CA">CA</option>
+          </select>
+         </div>
+
+         <div className="input-box">
+          <label>Descrição</label>
+          <textarea id="description"></textarea>
+         </div>
+
+         <div className="input-box">
+          <label >Status</label>
+          <select id="status">
+            <option value="pendente">Pendente</option>
+            <option value="Em-aberto">Em Aberto</option>
+            <option value="concluido">Concluido</option>
+            <option value="cancelado">Cancelado</option>
+          </select>
+         </div>
+
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
